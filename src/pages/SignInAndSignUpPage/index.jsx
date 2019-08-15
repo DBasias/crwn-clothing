12b13f76cc1936e6af0d12b84f1e9e10462e36c8
DBasias <dbasiadimas@hotmail.com>
2019-08-15 16:18:55 +0300

    added sign in and sign up page

---
+++ src/pages/SignInAndSignUpPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/SignIn';
+
+import './styles.scss';
+
+const SignInAndSingUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSingUpPage;
