0aca4c24058cd2b8758f794443cc21d0be0ea881
John Doe <dbasiadimas@hotmail.com>
2019-08-21 18:59:34 +0300

    moved state from ShopPage and Directory components to redux, created CollectionsOverview component to show shop items

--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import SHOP_DATA from './data';
+import CollectionsOverview from '../../components/CollectionsOverview';
 
-import CollectionPreview from '../../components/CollectionPreview';
-
-class ShopPage extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
